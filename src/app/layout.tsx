f3fcd0190d10840d7640744bc37c354508f4766b
RuanChenDa <21280103e0017@student.tdmu.edu.vn>
2025-10-02 14:58:04 +0700

    delete todo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        // TODO: Cần tối ưu layout cho màn hình nhỏ
         <Provider>
           <LangProvider>
           {children}
