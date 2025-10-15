094b37bab5d60878a515190077269a6150d87ece
RuanChenDa <21280103e0017@student.tdmu.edu.vn>
2025-10-15 22:32:01 +0700

    oke i see completely docker

---
+++ docker-test/server.js
@@ -0,0 +1,4 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => res.send("Hello Docker! my name dat"));
+app.listen(3000,() => console.log("Server running on port 3000"));
