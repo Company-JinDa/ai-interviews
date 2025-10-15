90e9b30e174040f25c53880d925703b72c924cce
RuanChenDa <21280103e0017@student.tdmu.edu.vn>
2025-10-15 22:18:06 +0700

    create docker test

---
+++ docker-test/server.ts
@@ -0,0 +1,10 @@
+// server.js
+import http from "http"
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Docker 🐳")
+})
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000")
+})
